d1e6ed84b06c3d3824ff034227bc997654c17779
Twisuki <Twisuki@outlook.com>
2025-04-18 17:56:40 +0800

    ds接口

--- src/ai/deepseek.js
+++ src/ai/deepseek.js
@@ -1,11 +1,28 @@
+const fs = require("fs");
+const chalk = require("chalk");
 import OpenAI from "openai";
 
-const apiUrl = 'https://api.deepseek.com/v1/chat/completions';
-
-class Deepseek {
-	constructor() {
-
-	}
+// 读取用户JSON
+let json = {};
+try {
+	const data = fs.readFileSync("../config/user.json", "utf8");
+	json = JSON.parse(data);
+} catch (err) {
+	console.error(chalk.red("Can't find user.json!"));
 }
 
-module.exports = Deepseek;
+
+// Deepseek接口
+const openai = new OpenAI({
+	baseURL: "https://api.deepseek.com",
+	apiKey: json.key
+});
+
+async function main() {
+	const completion = await openai.chat.completions.create({
+		messages: [{role: "system", content: json.content}],
+		model: "deepseek-chat",
+	});
+}
+
+main();
