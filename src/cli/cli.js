0af59c17aa5a2e86ca0143be0078f66645675121
Twisuki <Twisuki@outlook.com>
2025-04-19 16:02:56 +0800

    修复mode类型问题

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -11,13 +11,19 @@ class Cli {
 		console.log(chalk.green.bold("Kirafint Cli"));
 		console.log(chalk.blueBright("Mode: ", this.mode));
 
-		switch (this.mode) {
+		let mode = 0;
+		try {
+			mode = Number(this.mode);
+		} catch (err) {
+			console.error(chalk.red(`Undefined Parameter ${mode}`));
+		}
+		switch (mode) {
 			case 1:
 				const precision = new Precision();
 				await precision.chat();
 				break;
 			default:
-				console.error(chalk.red("Undefined Parameter!"));
+				console.error(chalk.red(`Undefined Parameter ${mode}`));
 				break;
 		}
 		// switch (this.mode) {
