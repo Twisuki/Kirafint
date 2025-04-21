6164bffb26efc2423ae2b595010f5d7f2fb789fe
Twisuki <Twisuki@outlook.com>
2025-04-21 23:55:58 +0800

    注释debug语句

--- src/mode/precision.js
+++ src/mode/precision.js
@@ -33,15 +33,15 @@ class Precision {
 		const msg = [];
 		// 初始化
 		const content = dsData.content.PrecisionMode.init.join("\n");
-		console.debug(chalk.greenBright("[INIT]", content));
+		// console.debug(chalk.greenBright("[INIT]", content));
 		msg.push({role: "user", content: content});
 
 		const response = await this.ds.chat(msg);
-		console.debug(chalk.blueBright("[INIT]", response));
+		// console.debug(chalk.blueBright("[INIT]", response));
 		msg.push({role: "assistant", content: response});
 
 		const initJson = await this.jsonAnalysis.getJson(response);
-		console.debug(chalk.yellowBright("[JSON]", JSON.stringify(initJson)));
+		// console.debug(chalk.yellowBright("[JSON]", JSON.stringify(initJson)));
 
 		while (1) {
 			// 获取输入
@@ -55,16 +55,16 @@ class Precision {
 
 			// 拼接问题
 			const qustionContent = userInput + "\n" + JSON.stringify(indexData.index);
-			console.debug(chalk.greenBright("[INDEX]", qustionContent));
+			// console.debug(chalk.greenBright("[INDEX]", qustionContent));
 			msg.push({role: "user", content: qustionContent});
 
 			const qustionResponse = await this.ds.chat(msg);
-			console.debug(chalk.blueBright("[INDEX]",qustionResponse));
+			// console.debug(chalk.blueBright("[INDEX]",qustionResponse));
 			msg.push({role: "assistant", content: qustionResponse});
 
 			// 获取文章
 			const indexJson = await this.jsonAnalysis.getJson(qustionResponse);
-			console.debug(chalk.yellowBright("[JSON]", JSON.stringify(indexJson)));
+			// console.debug(chalk.yellowBright("[JSON]", JSON.stringify(indexJson)));
 
 			const dataNeeded = [];
 			for (const key in indexJson.dataNeeded) {
@@ -82,16 +82,16 @@ class Precision {
 
 			// 上传文章
 			const dataContent = JSON.stringify(dataNeeded);
-			console.debug(chalk.greenBright("[DATA]", dataContent));
+			// console.debug(chalk.greenBright("[DATA]", dataContent));
 			msg.push({role: "user", content: dataContent});
 
 			// 获取回复
 			const answerResponse = await this.ds.chat(msg);
-			console.debug(chalk.blueBright("[INDEX]", answerResponse));
+			// console.debug(chalk.blueBright("[INDEX]", answerResponse));
 			msg.push({role: "assistant", content: answerResponse});
 
 			const answerJson = await this.jsonAnalysis.getJson(answerResponse);
-			console.debug(chalk.yellowBright("[JSON]", JSON.stringify(answerJson)));
+			// console.debug(chalk.yellowBright("[JSON]", JSON.stringify(answerJson)));
 
 			console.log(chalk.cyanBright("$ Kirafint > ", answerJson.msgResponse));
 		}
