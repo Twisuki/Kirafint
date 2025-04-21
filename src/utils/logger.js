8ad0032da95fa3f90448f7da9802b695fd802935
Twisuki <Twisuki@outlook.com>
2025-04-22 02:01:06 +0800

    log自定义颜色字段

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,19 +3,24 @@ const JsonLoader = require("../config/jsonLoader");
 const userData = JsonLoader.getJson("user");
 
 module.exports = {
-    info(msg) {
-        if (userData.log.info) {
-            console.log(chalk.blueBright("[INFO] "), msg);
-        }
-    },
-    debug(msg) {
-        if (userData.log.debug) {
-            console.log(chalk.yellowBright("[DEBUG] "), msg);
-        }
-    },
-    error(msg) {
-        if (userData.log.error) {
-            console.log(chalk.redBright("[ERROR] "), msg);
-        }
-    }
+	info (msg) {
+		if (userData.log.info) {
+			console.log(chalk.blueBright("[INFO] "), msg);
+		}
+	},
+	debug (msg) {
+		if (userData.log.debug) {
+			console.log(chalk.yellowBright("[DEBUG] "), msg);
+		}
+	},
+	error (msg) {
+		if (userData.log.error) {
+			console.log(chalk.redBright("[ERROR] "), msg);
+		}
+	},
+	custom (label, color, msg) {
+		if (userData.log.custom) {
+			console.log(chalk.hex(color)(`[${label}] `), msg);
+		}
+	}
 };
