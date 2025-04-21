3aac154926d4d43ee9f090b650c77b98a2b36315
Twisuki <Twisuki@outlook.com>
2025-04-22 01:26:42 +0800

    重写logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,20 +2,20 @@ const chalk = require("chalk");
 const JsonLoader = require("../config/jsonLoader");
 const userData = JsonLoader.getJson("user");
 
-export function info (msg) {
-	if (userData.log) {
-		console.log(chalk.blueBright("[INFO] "), msg);
-	}
-}
-
-export function debug (msg) {
-	if (userData.log) {
-		console.log(chalk.yellowBright("[DEBUG] "), msg);
-	}
-}
-
-export function error (msg) {
-	if (userData.log) {
-		console.log(chalk.redBright("[ERROR] "), msg);
-	}
-}
+module.exports = {
+    info(msg) {
+        if (userData.log.info) {
+            console.log(chalk.blueBright("[INFO] "), msg);
+        }
+    },
+    debug(msg) {
+        if (userData.log.debug) {
+            console.log(chalk.yellowBright("[DEBUG] "), msg);
+        }
+    },
+    error(msg) {
+        if (userData.log.error) {
+            console.log(chalk.redBright("[ERROR] "), msg);
+        }
+    }
+};
