396f5261f1272f652a643a675b60db6ca549f257
Twisuki <Twisuki@outlook.com>
2025-04-22 01:21:05 +0800

    export function

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,19 +2,19 @@ const chalk = require("chalk");
 const JsonLoader = require("../config/jsonLoader");
 const userData = JsonLoader.getJson("user");
 
-function info (msg) {
+export function info (msg) {
 	if (userData.log) {
 		console.log(chalk.blueBright("[INFO] "), msg);
 	}
 }
 
-function debug (msg) {
+export function debug (msg) {
 	if (userData.log) {
 		console.log(chalk.yellowBright("[DEBUG] "), msg);
 	}
 }
 
-function error (msg) {
+export function error (msg) {
 	if (userData.log) {
 		console.log(chalk.redBright("[ERROR] "), msg);
 	}
