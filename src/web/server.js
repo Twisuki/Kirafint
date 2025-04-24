6cb2b0084f093cb1f17dc5c1c14399498cbd38b8
Twisuki <Twisuki@outlook.com>
2025-04-24 18:07:54 +0800

    测试server

--- src/web/server.js
+++ src/web/server.js
@@ -1,1 +1,9 @@
 const WebSocket = require("ws");
+const logger = require("../utils/logger")
+
+const wss = new WebSocket.Server({ port: 2750 });
+
+// 连接处理
+wss.on("connection", (ws) => {
+	logger.debug("logger: 客户端已连接");
+});
