333b03eabd403afb7d6024d08899ebc6df46e556
Twisuki <Twisuki@outlook.com>
2025-04-24 18:09:52 +0800

    修改index.js为web server入口

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 #!/usr/bin/env node
 
-const {Command} = require("commander");
-const Cli = require("./cli/cli");
-
-const program = new Command();
-program
-	.name("Kirafint")
-	.description("Kirafint, 基于Deepseek API和本地模型的命令行AI Demo")
-	.version("0.0.1")
-	.requiredOption("-m, --mode <mode>", "Kirafint Mode")
-	.action((options) => {
-		const cli = new Cli(options.mode);
-		cli.start();
-	});
-
-program.parse(process.argv);
+const Server = require("./web/server");
