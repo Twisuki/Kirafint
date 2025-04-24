dcd1fdfec16f76b500c92c8668a9561934dc5d4f
Twisuki <Twisuki@outlook.com>
2025-04-24 18:19:18 +0800

    移除无用包

--- src/web/server.js
+++ src/web/server.js
@@ -8,12 +8,14 @@ const wss = new WebSocket.Server({ port: 2750 });
 wss.on("connection", (ws) => {
 	logger.info("客户端已连接");
 
+	// 接收消息
 	ws.on("message", (msg) => {
 		logger.info(`收到消息: ${msg}`);
 
 		ws.send(`服务器收到消息: ${msg}`);
 	});
 
+	// 连接关闭
 	ws.on("close", () => {
 		logger.info("用户已断开连接");
 	})
