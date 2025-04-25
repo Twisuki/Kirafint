7953725aa7215507f6bb5282b26878cab5afa099
Twisuki <Twisuki@outlook.com>
2025-04-25 17:38:43 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,15 +1,37 @@
 const logger = require("./utils/logger");
+const Precision = require("./mode/precision");
 
-async function main (io) {
+class Main {
+	constructor (io) {
+		this.io = io;
+	}
 
+	async start () {
+		logger.info("Kirafint Cli");
+
+		const mode = Number(await this.io.input());
+		logger.info(`Mode: ${mode}`);
+
+		switch (mode) {
+			case 1:
+				const Precision = require("./mode/precision");
+				const precision = new Precision();
+				await precision.chat();
+				break;
+
+			default:
+				logger.error(`Undefined Parameter ${mode}`);
+				break;
+		}
+	}
+
+	async getInput () {
+
+	}
+
+	async output () {
+
+	}
 }
 
-async function getInput () {
-
-}
-
-async function output () {
-
-}
-
-module.exports =  {main};
+module.exports =  Main;
