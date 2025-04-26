e97b055740614f2e2e894f4ec2999ede3407c3dd
Twisuki <Twisuki@outlook.com>
2025-04-26 15:27:36 +0800

    手动恢复index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+const {Command} = require("commander");
+const Cli = require("./io/cli");
 const Server = require("./io/server");
+
+const program = new Command();
+
+program.action(() => {
+	const cli = new Cli();
+	cli.start();
+});
+
+program.parse(process.argv);
