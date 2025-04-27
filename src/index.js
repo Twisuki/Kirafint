350419e425a472cafe420800e11ba74737aadb45
Twisuki <Twisuki@outlook.com>
2025-04-27 16:17:57 +0800

    重写基于commander的index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,21 @@ const Server = require("./io/server");
 
 const program = new Command();
 
-program.action(() => {
-	if (0) {
-		const cli = new Cli();
-		cli.start();
-	} else {
-		const server = new Server();
-		server.start();
-	}
-});
+program
+	.name("Kirafint")
+	.description("基于Deepseek API和本地模型的AI Demo")
+	.option("-c, --cli", "命令行模式")
+	.option("-s, --server", "服务器模式")
 
 program.parse(process.argv);
+
+const options = program.opts();
+if (options.cli) {
+	const cli = new Cli();
+	cli.start();
+} else if (options.server) {
+	const server = new Server();
+	server.start();
+} else if (Object.keys(options).length === 0) {
+	program.help();
+}
