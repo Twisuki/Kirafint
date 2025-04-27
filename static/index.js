de6c52f19865049522417bd45a622fcf3e10c130
Twisuki <Twisuki@outlook.com>
2025-04-27 18:58:32 +0800

    逻辑迁移到js

--- static/index.js
+++ static/index.js
@@ -1,7 +1,40 @@
-
 function logger (msg) {
 	const newDiv = `<div class="log"><span>[LOG]</span>\n${msg}</div>`;
 	document.getElementById("chat-box").insertAdjacentHTML('beforeend', newDiv);
 }
 
-setInterval(() => logger("尝试连接服务器: 2750..."), 1000);
+function getMsg (msg) {
+	const newDiv = `<div class="chat chat-server"><span>[SERVER]</span>\n${msg}</div>`;
+	document.getElementById("chat-box").insertAdjacentHTML('beforeend', newDiv);
+}
+
+function sendMsg (msg) {
+	const newDiv = `<div class="chat chat-user"><span>[USER]</span>\n${msg}</div>`;
+	document.getElementById("chat-box").insertAdjacentHTML('beforeend', newDiv);
+}
+
+const socket = new WebSocket("ws://localhost:2750");
+
+// 连接成功
+socket.onopen = function (event) {
+	logger("连接已建立.");
+}
+
+// 接收消息
+socket.onmessage = function (event) {
+	getMsg(event.data);
+}
+
+// 关闭连接
+socket.onclose = function (event) {
+	if (event.wasClean) {
+		logger("连接已关闭.");
+	} else {
+		logger(`连接异常关闭, Code: ${event.code} Reason: ${event.reason}`);
+	}
+}
+
+// 错误
+socket.onerror = function (error) {
+	logger("WebSocket错误: ", error);
+};
